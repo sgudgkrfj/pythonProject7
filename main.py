c6ef0718f178525a203915c8577bdc762e8e45cb
sgudgkrfj <131796501+sgudgkrfj@users.noreply.github.com>
2023-05-02 18:41:47 +0300

    перший приклад обгортки функції

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#теорія Декоратори
+
+def my_decorator_func(func):#3
+    def wrapper():#4
+        print("шото там")
+        func()
+        print("і там шось")
+    return wrapper
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@my_decorator_func#2
+def say_hello():#1
+    print("Hello!")
+say_hello()
